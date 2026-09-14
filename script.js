02bc60d4cc75ac2c546ac72aa503731c49d13b60
Lucas Cunha <lucas.8765350@aluno.mg.gov.br>
2026-09-14 18:01:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirmenu(){
+    const menu = document.querySelector(".menu");
+    menu.style.display = "flex";
+}
+
+function fecharmenu(){
+    const menu = document.querySelector(".menu");
+    menu.style.display = "none";
+}
