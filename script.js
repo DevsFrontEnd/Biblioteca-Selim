53b2ea8fb86b61771dab5bb9de46edd7d32e24ec
Lucas Cunha <lucas.8765350@aluno.mg.gov.br>
2026-09-14 15:08:21 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function abrirmenu(){
-    const menu = document.querySelector(".menu");
-    menu.style.display = "flex";
-}
-
-function fecharmenu(){
-    const menu = document.querySelector(".menu");
-    menu.style.display = "none";
-}
