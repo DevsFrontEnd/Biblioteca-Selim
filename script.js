9372887e8aa2ec1d84833f87835eb1a0971f7f51
gustavo11795259 <gustavo.11795259@aluno.mg.gov.br>
2026-09-15 15:26:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,141 @@ function fecharmenu(){
     const menu = document.querySelector(".menu");
     menu.style.display = "none";
 }
+
+const livros = {
+    Livro1: {
+        nome: "Depois Daquela Viagem",
+        imagem: "",
+        autor: "Valéria Polizzi",
+        isbn: "978-8508164295",
+        editora: "Edição Ática",
+        paginas: "288 Páginas",
+        genero: "Biografia"
+    },
+    Livro2: {
+        nome: "Eu sou Malala",
+        imagem: "",
+        autor: "Malala Yousafzai",
+        isbn: "978-8535923438",
+        editora: "Edição Companhia das Letras",
+        paginas: "360 Páginas",
+        genero: "Biografia, Autobiografia"
+    },  
+    Livro3: {
+        nome: "Memórias Póstumas de Brás Cubas",
+        imagem: "",
+        autor: "Machado de Assis",
+        isbn: "978-8582850015",
+        editora: "Edição Penguin/Companhia das Letras",
+        paginas: "368 Páginas",
+        genero: "Romance e Ficção"
+    },
+    Livro4: {
+        nome: "Lucíola",
+        imagem: "",
+        autor: "José de Alencar",
+        isbn: "978-8508126996",
+        editora: "Edição Ática",
+        paginas: "144 Páginas",
+        genero: "Ficção"
+    },
+    Livro5: {
+        nome: "Iracema",
+        imagem: "",
+        autor: "José de Alencar",
+        isbn: "978-8582850336",
+        editora: "Edição Penguin/Companhia das Letras",
+        paginas: "200 Páginas",
+        genero: "Ficção"
+    },
+    Livro6: {
+        nome: "Bíblia Linux",
+        imagem: "",
+        autor: "Christopher Negus",
+        isbn: "978-8576087991",
+        editora: "Alta Books",
+        paginas: "856 Páginas",
+        genero: "Informática e Tecnologia"
+    },
+    Livro7: {
+        nome: "Entendendo Algoritmos",
+        imagem: "",
+        autor: "Aditya Y. Bhargava",
+        isbn: "978-8575226629",
+        editora: "Novatec Editora",
+        paginas: "264 Páginas",
+        genero: "Informática e Tecnologia"
+    },
+    Livro8: {
+        nome: "Java para Leigos",
+        imagem: "",
+        autor: "Barry A. Burd",
+        isbn: "978-8576088011",
+        editora: "AltaBooks",
+        paginas: "424 Páginas",
+        genero: "Informática e Tecnologia"
+    },
+    Livro9: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+    Livro10: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+    Livro11: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+    Livro12: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+    Livro13: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+    Livro14: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+    Livro15: {
+        nome: "",
+        imagem: "",
+        autor: "",
+        isbn: "",
+        editora: "",
+        paginas: "",
+        genero: ""
+    },
+}
